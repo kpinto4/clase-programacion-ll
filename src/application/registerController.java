c0edebb18ccf6046caf9556764b9916033d6d4d3
kevin <kevin@DESKTOP-EEK7GOH>
2024-03-05 11:04:27 -0500

    generate controllers

---
+++ src/application/registerController.java
@@ -0,0 +1,21 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+public class registerController {
+	@FXML
+	private TextField txtUerR;
+	@FXML
+	private TextField txtPasswR1;
+	@FXML
+	private TextField txtPasswR2;
+	@FXML
+	private Button btnRegis;
+	@FXML
+	private Button btnCloseR;
+
+}
