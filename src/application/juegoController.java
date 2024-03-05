4a919157d070dbcfdcacfb8000953dbb47114fa7
kevin <kevin@DESKTOP-EEK7GOH>
2024-03-05 10:40:34 -0500

    Primer commit

---
+++ src/application/juegoController.java
@@ -0,0 +1,81 @@
+package application;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.image.ImageView;
+
+public class juegoController {
+	@FXML
+	private ImageView imgJug;
+	@FXML
+	private TextField txtL1;
+	@FXML
+	private TextField txtL2;
+	@FXML
+	private TextField txtL3;
+	@FXML
+	private TextField txtL4;
+	@FXML
+	private TextField txtL5;
+	@FXML
+	private TextField txtL6;
+	@FXML
+	private Button A;
+	@FXML
+	private Button B;
+	@FXML
+	private Button C;
+	@FXML
+	private Button D;
+	@FXML
+	private Button E;
+	@FXML
+	private Button F;
+	@FXML
+	private Button G;
+	@FXML
+	private Button H;
+	@FXML
+	private Button I;
+	@FXML
+	private Button J;
+	@FXML
+	private Button K;
+	@FXML
+	private Button L;
+	@FXML
+	private Button M;
+	@FXML
+	private Button N;
+	@FXML
+	private Button Ñ;
+	@FXML
+	private Button O;
+	@FXML
+	private Button P;
+	@FXML
+	private Button Q;
+	@FXML
+	private Button R;
+	@FXML
+	private Button S;
+	@FXML
+	private Button T;
+	@FXML
+	private Button U;
+	@FXML
+	private Button V;
+	@FXML
+	private Button W;
+	@FXML
+	private Button X;
+	@FXML
+	private Button Y;
+	@FXML
+	private Button Z;
+
+}
---
+++ src/application/user.java
@@ -0,0 +1,23 @@
+package application;
+
+public class user {
+    private String username;
+    private String password;
+
+    public user(String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
